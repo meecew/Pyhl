fa3f4c4c15e854610808e8f576cec31dbb21448a
meecew <165352690+meecew@users.noreply.github.com>
2024-06-13 22:10:21 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,16 @@
 # ----------
 # HOW TO USE:
-# LINE 159 CHANGE movePerDegree TO CORRECT VALUE
-# PRESS ALT TO TOGGLE ON / OFF
-# CHANGE TOGGLE KEY ON LINE 72
+# LINE 64 CHANGE movePerDegree TO CORRECT VALUE
+# HOLD ALT TO TRACK TARGET
+# V TO SWITCH TARGET
+# CHANGE KEYS ON LINE 69 AND 70
 # ----------
-
-
-import keyboard, time, threading, sys, random, ctypes, math, os
+import keyboard, time, threading, sys, random, ctypes, math
 from pynput.mouse import Controller, Button
 from win32gui import GetWindowText, GetForegroundWindow
  
 from offsets import *
-import pyMeow as pm 
+import pyMeow as pm # type: ignore
 
 mouse = Controller()
 client = Client()
@@ -24,7 +23,6 @@ class Offsets:
     dwLocalPlayerController = client.offset('dwLocalPlayerController')
     dwViewMatrix = client.offset('dwViewMatrix')
     dwViewAngles = client.offset('dwViewAngles')
-
 
     m_pGameSceneNode = client.clientdll('C_BaseEntity', 'm_pGameSceneNode')
     m_vecVelocity = client.clientdll('C_BaseEntity', 'm_vecVelocity')
@@ -43,7 +41,7 @@ class Entity:
     @property
     def health(self):
         return pm.r_int(self.pr, self.entity + Offsets.m_iHealth)
-    
+
     @property
     def team(self):
         return pm.r_int(self.pr, self.entity + Offsets.m_iTeamNum)
@@ -55,22 +53,25 @@ class Entity:
     @property
     def velocity(self):
         return pm.r_float(self.pr, self.entity + Offsets.m_vecVelocity)
-        
-
-
 
 class Colors:
     white = pm.get_color("white")
     black = pm.get_color("black")
 
-
-class Pyhl:
+class CoClass:
     def __init__(self):
+        # moveperdegree = 100 / sensitivity * 2.3
+        self.movePerDegree = 100 / 2.3
         self.pr = pm.open_process("cs2.exe") # process
         self.client = pm.get_module(self.pr, "client.dll")["base"] # client starting point
 
         self.mode = 0
         self.triggerKey = "alt"
+        self.switchTarget = 'v'
+
+        self.gettingId = False
+        self.entIdList = []
+        self.temp = 0
         
         print(f"Trigger key: {self.triggerKey.upper()}")
 
@@ -83,12 +84,9 @@ class Pyhl:
                 continue
 
             if keyboard.is_pressed(self.triggerKey):
-                while keyboard.is_pressed(self.triggerKey):
-                    pass
-                if self.mode == 0:
-                    self.mode = 1
-                else: 
-                    self.mode = 0
+                self.mode = 1
+            else: 
+                self.mode = 0
             time.sleep(0.15)
     
     def triggerBot(self):
@@ -101,36 +99,34 @@ class Pyhl:
                 time.sleep(0.3)
                 continue
             
-
-            try:
-                player = pm.r_int64(self.pr, self.client + Offsets.dwLocalPlayerPawn)
-                entityId = pm.r_int(self.pr, player + Offsets.m_iIDEntIndex)
-
-                if entityId > 0:
-                    entList = pm.r_int64(self.pr, self.client + Offsets.dwEntityList)
-                    entEntry = pm.r_int64(self.pr, entList + 0x8 * (entityId >> 9) + 0x10)
-                    entity = pm.r_int64(self.pr, entEntry + 120 * (entityId & 0x1FF))
-                    ent = Entity(self.pr, entity)
-
-                    playerTeam = pm.r_int(self.pr, player + Offsets.m_iTeamNum)
-
-                    if ent.team != playerTeam:  # NORMAL MATCHES
-                    #if True: # DEATH MATCH
-                        if ent.health > 0: 
-                            selfpos = pm.r_vec3(self.pr, player + Offsets.m_vOldOrigin)
-                            self.hl(ent.position, selfpos)
-                                
+            for ent in self.checkEnt(): 
+                try:
+                    while ent.health > 0:
+                        if keyboard.is_pressed(self.switchTarget):
+                            while keyboard.is_pressed(self.switchTarget):
+                                pass
+                            break
+                        try:
+                            if self.mode == 0:
+                                raise
+                            player = pm.r_int64(self.pr, self.client + Offsets.dwLocalPlayerPawn)
+                            playerTeam = pm.r_int(self.pr, player + Offsets.m_iTeamNum)
+                            #if ent.team != playerTeam:  # NORMAL MATCHES
+                            if True: # DEATH MATCH
+                                if ent.health > 0:
+                                    selfpos = pm.r_vec3(self.pr, player + Offsets.m_vOldOrigin)
+                                    self.hl(ent.position, selfpos)
+                                else:
+                                    break
+                        except:
                             time.sleep(0.01)
-                            mouse.press(Button.left)
-                            time.sleep(0.02)
-                            mouse.release(Button.left)
-                time.sleep(0.03)
-            except:
-                time.sleep(0.03)
 
-    def headlock(self, entpos, selfpos):
-         
+                        time.sleep(0.01)
+                except:
+                    pass
+                
         
+    def headlock(self, entpos, selfpos):
         selfx = selfpos['x']
         selfy = selfpos['y']
         selfz = selfpos['z']
@@ -148,7 +144,7 @@ class Pyhl:
         if entx - selfx < 0:
             theta = 180 - theta
 
-        targetYaw = theta # - viewAngle[1]
+        targetYaw = theta 
         while targetYaw > 180:
             targetYaw -= 360
         while targetYaw < -180:
@@ -156,11 +152,10 @@ class Pyhl:
 
         return targetPitch, targetYaw
 
+
+
     def hl(self, entpos, selfpos):
-        # moveperdegree = 100 / sensitivity * 2.2
-
-        movePerDegree = 100 / 2.2
-
+        movePerDegree = self.movePerDegree
         viewAngle = pm.r_floats(self.pr, self.client + Offsets.dwViewAngles, 3) # PITCH, YAW, ROLL
         pitch = viewAngle[0]
         yaw = viewAngle[1]
@@ -174,7 +169,9 @@ class Pyhl:
             ctypes.c_uint(0),
             ctypes.c_uint(0)
         )
-      
+
+            
+
     def paint(self):
         pm.overlay_init(fps=144)
         while pm.overlay_loop():
@@ -190,23 +187,93 @@ class Pyhl:
             else:
                 pm.draw_text('OFF', 1200, 0, 50, Colors.white)
 
+            if self.gettingId == True:
+                pm.draw_text('Getting Players IDs...', 50, 0, 20, Colors.white)
+            else:
+                pass
+
+
             pm.end_drawing()
+            
+    def checkEnt(self):
+        entList = pm.r_int64(self.pr, self.client + Offsets.dwEntityList)
+
+        if len(self.entIdList) > 0:
+            for i in self.entIdList:
+                try:
+                    entEntry = pm.r_int64(self.pr, entList + 0x8 * (i >> 9) + 0x10)
+                    entity = pm.r_int64(self.pr, entEntry + 120 * (i & 0x1FF))
+                except:
+                    continue
+                yield Entity(self.pr, entity)
+
+    def checkEnttoo(self):
+        entList = pm.r_int64(self.pr, self.client + Offsets.dwEntityList)
+        for i in self.entIdList:
+            try:
+                entEntry = pm.r_int64(self.pr, entList + 0x8 * (i >> 9) + 0x10)
+                entity = pm.r_int64(self.pr, entEntry + 120 * (i & 0x1FF))
+    
+            except:
+                continue
+            yield Entity(self.pr, entity)
+
+
+    def getEntId(self):
+        while True:
+            if not GetWindowText(GetForegroundWindow()) == "Counter-Strike 2":
+                time.sleep(2)
+                continue
+
+            #if keyboard.is_pressed(self.getEntIdKey):
+            if True:
+                self.entIdList = []
+                self.gettingId = True
+                try:
+                    player = pm.r_int64(self.pr, self.client + Offsets.dwLocalPlayerPawn)
+                    playerTeam = pm.r_int(self.pr, player + Offsets.m_iTeamNum)
+                except:
+                    pass
+                for i in range(1, 5000):
+                    try:
+                        entList = pm.r_int64(self.pr, self.client + Offsets.dwEntityList)
+                        entEntry = pm.r_int64(self.pr, entList + 0x8 * (i >> 9) + 0x10)
+                        entity = pm.r_int64(self.pr, entEntry + 120 * (i & 0x1FF))
+                        entTeam = pm.r_int(self.pr, entity + Offsets.m_iTeamNum)
+                        if pm.r_int(self.pr, entity + Offsets.m_iHealth) <= 0:
+                            continue
+    
+                        #if entTeam != playerTeam:  # NORMAL MATCHES / ONLY ENEMY HP
+                        if True: # DEATH MATCH / TEAM HEALTH
+                            self.entIdList.append(i)
+                    except:
+                        pass
+                time.sleep(0.5)
+                self.gettingId = False
+
+            time.sleep(10)
+
+   
         
 if __name__ == '__main__':
     print('Connecting...')
     time.sleep(1)
     os.system('cls')
-    pyhl = Pyhl()
+    coclass = CoClass()
 
-    oot = threading.Thread(target=pyhl.onoff)
-    tbt = threading.Thread(target=pyhl.triggerBot)
-    pt = threading.Thread(target=pyhl.paint)
+    cet = threading.Thread(target=coclass.getEntId)
+    oot = threading.Thread(target=coclass.onoff)
+    tbt = threading.Thread(target=coclass.triggerBot)
+    pt = threading.Thread(target=coclass.paint)
 
+    
 
+    cet.start()
     oot.start()
     tbt.start()
     pt.start()
 
+    cet.join()
     oot.join()
     tbt.join()
     pt.join()
